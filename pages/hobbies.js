7219f222fb01a7e15def025b1b8d55bbe055cf08
Johnathan Hoyt <jhoyt200931@yahoo.com>
2021-08-22 02:43:13 -0400

    started filling in profile

---
+++ pages/hobbies.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Hobbies() {
+    return (
+        <div>
+            <Link href='/'>
+                <a>Home</a>
+            </Link>
+        </div>
+    )
+}
