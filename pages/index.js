1cad74f06db86e1e1a3d3e36b0614fd097244439
Johnathan Hoyt <jhoyt200931@yahoo.com>
2021-08-22 14:00:17 -0400

    filled out about and hobby pages, added some styling

--- pages/index.js
+++ pages/index.js
@@ -42,13 +42,17 @@ export default function Home() {
 
         <p className={styles.description}>
           Let's start with a brief account of my journey into development:
-          <br />
+        </p>
+        <p className={styles.description}>
           My journey first started 3.5 years ago while I was still working customer service for a furniture company based in the state of New York. I was starting to get bored with this line of work and wanted to get into something more technical and challenging, so I started teaching myself how to code in my spare time.
-          <br />
+        </p>
+        <p className={styles.description}>
           Eventually, I got far enough into JavaScript to start on React. It was at this point that I realized that teacing myself was not going to cut it and I decided that I needed a more formal education on the matter.
-          <br />
+        </p>
+        <p className={styles.description}>
           After this, I was biding my time, waiting for the right opportunity to come along. After 2.5 years, that opportunity came in the form of Lambda School. Once I had heard of Lambda School, I decided it was time and I signed up. What followed was probably the most challenging, rewarding and fun educational experience I have ever endured.
-          <br />
+        </p>
+        <p className={styles.description}>
           Throughout my time at Lambda School, I learned HTML5, CSS3(and some preprocessors), vanilla JavaScript, React.js, Redux, Node.js and Python3. The road was not easy, but I learned so much and my skillset grew by leaps and bounds. Bringing my to today, in the job hunt in a new, more technical field.
         </p>
       </main>
